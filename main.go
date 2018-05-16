cde46125f02b1f3d7b13ccaa82bd9720281c0751
kyamazawa <yamazawa@supersoftware.co.jp>
2018-05-17 02:21:16 +0900

    change port

--- main.go
+++ main.go
@@ -24,13 +24,12 @@ func main() {
 
 	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir(dir))))
 
-	fmt.Println("Listening on :8080")
-	http.ListenAndServe(":8080", r)
+	fmt.Println("Listening on :80")
+	http.ListenAndServe(":80", r)
 }
 
 // GET /
 func clustersGet(w http.ResponseWriter, r *http.Request) {
-	// out, _ := exec.Command("docker", "ps", "-a", "-f", "ancestor=capsulecloud/kube-factory").Output()
 	out, _ := exec.Command("docker", "ps", "-a", "-f", "ancestor=capsulecloud/kube-factory", "--format", "<td>{{.Names}}</td><td>{{.CreatedAt}}</td><td>{{.Status}}</td><td><form action=\"/clusters/{{.Names}}\" method=\"post\" class=\"ui center aligned\"><button class=\"ui red button\" type=\"submit\">Delete</button></form></td>").Output()
 	print(string(out))
 	executeTemplate(w, "template/list.html", map[string]interface{}{"test": template.HTML(strings.Replace(string(out), "\n", "<br/>", -1))})
