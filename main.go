694728595c38c8b2f8193e89eb4b91ec6e5bf1cc
kyamazawa <yamazawa@supersoftware.co.jp>
2018-05-17 12:47:10 +0900

    add show page & default random cluster_id

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"html/template"
+	"math/rand"
 	"net/http"
 	"os/exec"
 	"strings"
@@ -21,7 +22,7 @@ func main() {
 	r.HandleFunc("/", clustersGet)
 	r.HandleFunc("/clusters", clustersPost).Methods("POST")
 	r.HandleFunc("/clusters/{id}", clusterDelete).Methods("POST")
-
+	r.HandleFunc("/cluster/{id}", clusterGet)
 	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir(dir))))
 
 	fmt.Println("Listening on :80")
@@ -30,19 +31,31 @@ func main() {
 
 // GET /
 func clustersGet(w http.ResponseWriter, r *http.Request) {
-	out, _ := exec.Command("docker", "ps", "-a", "-f", "ancestor=capsulecloud/kube-factory", "--format", "<td>{{.Names}}</td><td>{{.CreatedAt}}</td><td>{{.Status}}</td><td><form action=\"/clusters/{{.Names}}\" method=\"post\" class=\"ui center aligned\"><button class=\"ui red button\" type=\"submit\">Delete</button></form></td>").Output()
+	out, _ := exec.Command("docker", "ps", "-a", "-f", "ancestor=capsulecloud/kube-factory", "--format", "<td><a href=\"/clusters/{{.Names}}\">{{.Names}}</a></td><td>{{.CreatedAt}}</td><td>{{.Status}}</td><td><form action=\"/clusters/{{.Names}}\" method=\"post\" class=\"ui center aligned\"><button class=\"ui red button\" type=\"submit\">Delete</button></form></td>").Output()
 	print(string(out))
-	executeTemplate(w, "template/list.html", map[string]interface{}{"test": template.HTML(strings.Replace(string(out), "\n", "<br/>", -1))})
+	executeTemplate(w, "template/list.html", map[string]interface{}{"data": template.HTML(strings.Replace(string(out), "\n", "<br/>", -1))})
 }
 
 // POST /clusters
 func clustersPost(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	clusterID := r.Form.Get("cluster-id")
+	if clusterID == "" {
+		clusterID = randomString(8)
+	}
 	fmt.Println(clusterID)
 	out, _ := exec.Command("/root/create.sh", clusterID).Output()
 	print(string(out))
 	http.Redirect(w, r, "/", http.StatusFound)
+}
+
+// GET /cluster/:id
+func clusterGet(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	clusterID := vars["id"]
+	fmt.Println(clusterID)
+	out, _ := exec.Command("docker", "logs", clusterID).Output()
+	executeTemplate(w, "template/show.html", map[string]interface{}{"data": strings.Replace(string(out), "\n", "<br/>", -1)})
 }
 
 // DELETE /clusters/:id
@@ -66,4 +79,14 @@ func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func randomString(n int) string {
+	var letter = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+
+	b := make([]rune, n)
+	for i := range b {
+		b[i] = letter[rand.Intn(len(letter))]
+	}
+	return string(b)
 }
